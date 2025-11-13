deb6a5e9dfe8d241c878fbeee235184e34575f8f
vduc9262-prog <vduc9262@gmail.com>
2025-11-13 23:09:34 +0700

    bai4ss17.cpp

--- bai4ss17.cpp
+++ bai4ss17.cpp
@@ -2,108 +2,34 @@
 #include <stdlib.h>
 #include <time.h>
 #include <string.h>
-#define MAX 500
-
-void displayMenu();
-void nhapChuoi(char str[], int size);
-void nhapKyTu(char *c);
-void xoaKyTu(char str[], char c);
 
 int main() {
-    char chuoi[MAX];
-    char kyTuXoa;
-    int luaChon;
-    int i;
+    char chuoi[1000];
+    char ky_tu_xoa;
+    int i, j, len;
 
-    for (i = 0; i < MAX; i++) {
-        chuoi[i] = '\0';
+    printf("Nhap vao mot chuoi bat ky: ");
+    fgets(chuoi, sizeof(chuoi), stdin);
+
+    len = strlen(chuoi);
+    if (chuoi[len - 1] == '\n') {
+        chuoi[len - 1] = '\0';
+        len--;
     }
 
-    do {
-        displayMenu();
-        printf(" => Chon chuc nang (1-4): ");
-        scanf("%d", &luaChon);
-        getchar();
+    printf("Nhap vao ky tu can xoa: ");
+    scanf(" %c", &ky_tu_xoa);
 
-        switch (luaChon) {
-            case 1:
-                nhapChuoi(chuoi, MAX);
-                printf("\n Da nhap chuoi thanh cong!\n");
-                break;
-
-            case 2:
-                if (chuoi[0] == '\0') {
-                    printf("\n Chuoi trong! Chua nhap du lieu.\n");
-                } else {
-                    printf("\n Chuoi hien tai: %s\n", chuoi);
-                }
-                break;
-
-            case 3:
-                if (chuoi[0] == '\0') {
-                    printf("\n Loi: Chua nhap chuoi! Hay chon 1 truoc.\n");
-                } else {
-                    nhapKyTu(&kyTuXoa);
-                    xoaKyTu(chuoi, kyTuXoa);
-                    printf("\n Da xoa tat ca ky tu '%c'\n", kyTuXoa);
-                    printf(" Ket qua: %s\n", chuoi);
-                }
-                break;
-
-            case 4:
-                printf("\n Cam on ban da su dung chuong trinh!\n");
-                printf(" Hen gap lai lan sau!\n\n");
-                break;
-
-            default:
-                printf("\n Lua chon khong hop le! Chi chon tu 1 den 4.\n");
+    j = 0;
+    for (i = 0; i < len; i++) {
+        if (chuoi[i] != ky_tu_xoa) {
+            chuoi[j] = chuoi[i];
+            j++;
         }
+    }
+    chuoi[j] = '\0';
 
-        if (luaChon != 4) {
-            printf("\n Nhan Enter de quay lai menu...");
-            getchar();
-        }
-
-    } while (luaChon != 4);
+    printf("Chuoi sau khi xoa ky tu '%c': %s\n", ky_tu_xoa, chuoi);
 
     return 0;
 }
-
-void displayMenu() {
-    system("cls");
-    printf("==================================\n");
-    printf("      XOA KY TU KHOI CHUOI        \n");
-    printf("==================================\n");
-    printf("1. Nhap chuoi moi\n");
-    printf("2. Hien thi chuoi hien tai\n");
-    printf("3. Xoa mot ky tu khoi chuoi\n");
-    printf("4. Thoat chuong trinh\n");
-    printf("----------------------------------\n");
-}
-
-void nhapChuoi(char str[], int size) {
-    printf("Nhap vao mot chuoi bat ky: ");
-    fgets(str, size, stdin);
-    str[strcspn(str, "\n")] = '\0';
-}
-
-void nhapKyTu(char *c) {
-    printf("Nhap ky tu can xoa: ");
-    scanf("%c", c);
-    getchar();
-}
-
-void xoaKyTu(char str[], char c) {
-    int i = 0;
-    int j = 0;
-
-    while (str[i] != '\0') {
-        if (str[i] != c) {
-            str[j] = str[i];
-            j++;
-        }
-        i++;
-    }
-    str[j] = '\0';
-}
-
